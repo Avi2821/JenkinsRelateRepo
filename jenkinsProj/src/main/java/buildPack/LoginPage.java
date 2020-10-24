24c42bea76740273f401d015c05d350350cbecaf
avinash <avinashnatikar28@gmail.com>
2020-10-24 10:10:48 +0530

    Colletion of browsers to run the code

--- jenkinsProj/src/main/java/buildPack/LoginPage.java
+++ jenkinsProj/src/main/java/buildPack/LoginPage.java
@@ -1,14 +1,33 @@
 package buildPack;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginPage {
 	
+	RemoteWebDriver driver;
+
 	@Test
 	public void loginSteps() throws InterruptedException {
+		
+		String browser = System.getProperty("browser");
+		System.out.print(browser);
+		if(browser.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}else if(browser.equals("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}else if(browser.equals("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		}
+	
 		
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver = new ChromeDriver();
